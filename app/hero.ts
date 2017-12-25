55e154d10193c26191dd83c31235333a006485c8
xiazhe <xiazhe@outlook.com>
2017-12-25 15:43:52 +0800

    remove

--- app/hero.ts
+++
@@ -1,7 +0,0 @@
-/**
- * Created by lucker.xia on 2/23/2016.
- */
-export interface Hero {
-    id: number;
-    name: string;
-}
--- app/main.ts
+++
@@ -1,6 +0,0 @@
-/**
- * Created by lucker.xia on 2/23/2016.
- */
-import {bootstrap}    from 'angular2/platform/browser'
-import {AppComponent} from './app.component'
-bootstrap(AppComponent);
